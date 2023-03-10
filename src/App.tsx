ec504a00d3e46f86485a35406f8a45d06dd56531
Ayush Kumar Sinha <ayush_snha@rediffmail.com>
2023-03-10 18:32:20 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import H1 from './App.styles';
+
+function App() {
+    return (
+        <div>
+            <H1>
+                React BoilerPlate
+            </H1>
+        </div>
+    );
+}
+
+export default App;
