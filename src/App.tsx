a413cafd2f2e363eca3ca469279b179df5a1597c
ayushsnha <ayush_snha@rediffmail.com>
2023-03-10 18:43:46 +0530

    remove styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import H1 from './App.styles';
 
 function App() {
     return (
         <div>
-            <H1>
+            <h1>
                 React BoilerPlate
-            </H1>
+            </h1>
         </div>
     );
 }
